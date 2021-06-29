c4b6236086e88ad1ac7623ffba2ebfa1da1caa85
lukmansp <lukmanssefriyanto@gmail.com>
2021-06-29 21:38:37 +0700

    llogiic

--- lib/ui/home_page_view.dart
+++ lib/ui/home_page_view.dart
@@ -6,13 +6,11 @@ import 'modules/profil/pages/profil_view.dart';
 import 'modules/reward/pages/reward_view.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   static List<Widget> _widgetOptions = <Widget>[
     CreditCard(),
     RewardView(),
@@ -20,6 +18,8 @@ class _HomePageState extends State<HomePage> {
   ];
   int _selectedIndex = 0;
   void _onItemTapped(int index) {
+    logicTriangle();
+    logicFibonaci();
     setState(() {
       _selectedIndex = index;
     });
@@ -32,24 +32,46 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.white,
-        title: Text(ConstantText.labelTitleHome,style: TextStyle(color: Colors.black),),
+        title: Text(
+          ConstantText.labelTitleHome,
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       body: Container(
         margin: EdgeInsets.all(30),
-        child: Center(
-            child:_widgetOptions.elementAt(_selectedIndex)
-        ),
+        child: Center(child: _widgetOptions.elementAt(_selectedIndex)),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
-        items:const<BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.credit_card_outlined),label: ConstantText.labelHomeCard),
-          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard_outlined),label: ConstantText.labelHomeReward),
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: ConstantText.labelHomeProfil),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.credit_card_outlined), label: ConstantText.labelHomeCard),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard_outlined), label: ConstantText.labelHomeReward),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: ConstantText.labelHomeProfil),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.blue,
         onTap: _onItemTapped,
-      ),// This trailing comma makes auto-formatting nicer for build methods.
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }}
+  }
+
+  void logicTriangle() {
+    for (int i = 1; i <= 5; i++) {
+      print('*' * i);
+    }
+  }
+  void logicFibonaci(){
+    int begin =0;
+    int end =1;
+    int count =20;
+    for(int i =0;i <= count; i ++){
+      print('$begin');
+      int sum = begin + end;
+      begin = end;
+      end = sum;
+    }
+  }
+}
